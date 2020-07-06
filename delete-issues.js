8d46c342c288bedb1e17c7363a827d09f7d1ecbd
Noah Thorp <noah@comakery.com>
2020-07-05 20:42:19 -0700

    delete issues script

---
+++ delete-issues.js
@@ -0,0 +1,56 @@
+require('dotenv').config()
+const { graphql } = require("@octokit/graphql")
+
+const auth_token = process.env.GITHUB_PERSONAL_ACCESS_TOKEN
+const issuesToDelete = 100
+
+async function deleteIssue(issueId) {
+  await graphql(
+    `
+    mutation DeleteIssueInput {
+        deleteIssue(input: {issueId:"${issueId}" }) {
+            clientMutationId
+        }
+      }
+    `,
+    {
+      headers: {
+        authorization: `token ${auth_token}`,
+      },
+    }
+  ).then(x => console.log(x))
+  .catch(err => {
+      console.log(err)
+  })
+}
+
+graphql(
+    `
+      {
+        repository(owner: "CoMakery", name: "comakery-app") {
+          issues(last: ${issuesToDelete}) {
+            edges {
+              node {
+                id
+                title
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      headers: {
+        authorization: `token ${auth_token}`,
+      },
+    }
+  ).then((response) => {
+      let edges = response.repository.issues.edges
+      return edges.map(issue => issue.node.id)
+}).then((issueIds) => {
+  let deletions = issueIds.map(issueId => deleteIssue(issueId))
+  console.log(`deleting ${deletions.length} issues` )
+  Promise.all(deletions)
+})
+.catch(err => console.log(err))
+
